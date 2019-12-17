84020cdcc66a986137836910e3a224e0bcdc2b09
Fahad Abbas <adventivepk@gmail.com>
2019-12-17 22:08:42 +0500

    modified : index page search bar tool bar added

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,47 @@
 <template>
-    <v-content>
-      <v-container
-        class="fill-height"
-        fluid
-      >
-        <v-row
-          align="center"
-          justify="center"
-        >
-          <v-col class="text-center">
-            {{appName}}
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-content>
+  <v-content>
+    <v-container class="fill-height" fluid>
+      <v-row no-gutters>
+        <v-col cols="12" sm="6">
+          <p>Search any of the the image</p>
+          <v-text-field 
+            @keyup="searchGiphy" 
+            v-model="SearchQuery" 
+            clearable
+            hide-no-data
+            hide-details
+            >
+          </v-text-field>
+        </v-col>
+        <v-col cols="12" sm="6">
+          <div>
+            Selected Images
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-content>
 </template>
-
-
-
 <script>
 import { mapMutations } from 'vuex'
 
 export default {
   data () {
     return {
-      appName : this.$store.getters.counter
+      appName : this.$store.getters.counter,
+      SearchQuery:[],
     }
   },
   methods : {
-
+    searchGiphy (e) {
+      let query = this.SearchQuery.trim()
+      console.log(query)
+    }
   },
   beforeMount () {
-    console.log('before mounted');
   },
   mounted () {
-    console.log('mounted');
+    this.$myFunction('this my app again')
   }
 }
 
