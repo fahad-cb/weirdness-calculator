0a17cde1a27254465d44b437d693710ac879a290
fahad-cb <adventivepk@gmail.com>
2019-12-17 09:25:01 +0500

    added : components and layout added in source

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<!-- Main Component(Controls the whole app) -->
+<template>
+  <v-app dark>
+    <appHeader/>
+    <nuxt /> 
+    <appFooter/>
+  </v-app>
+</template>
+<script>
+
+
+
+export default {
+    layout : 'default',
+    components : {
+        appHeader : () => import ('~/components/appHeader'),
+        appFooter : () => import ('~/components/appFooter'),
+    }
+}
+</script>
