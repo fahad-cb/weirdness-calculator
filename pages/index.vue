f6db2544f2adff97ed5befe003a2310b14e243ea
Fahad Abbas <adventivepk@gmail.com>
2019-12-18 21:53:47 +0500

    modified: search image complete and range slider added for weirdness

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <v-content>
     <v-container  >
-      <v-row no-gutters>
+      <v-row >
         <v-col cols="12" sm="6" vs-type="flex">
           <p>Find out how weird you are by selecting GIFs that make you laugh. We'll show you the least weird ones to start, but you can move the slider to make them weirder.</p>
           <p>When you find a GIF you like, press the like button. Once you like 5 GIFs, we'll show you how weird are you.</p>
-          <v-row no-gutters>
+          <v-row >
             <v-col cols="12" sm="8">
               <v-text-field 
                 v-model="SearchQuery" 
@@ -20,10 +20,30 @@
             </v-col>
           </v-row>
           <v-divider></v-divider>
-          <v-row no-gutters>
-            <v-col cols="12" sm="8" class="text-center">
-                <v-img v-if="selectedImage" :src="`${selectedImage}`"></v-img>
+          <v-row>
+            <v-progress-linear
+               v-if="searchLoading"
+              :active="true"
+              :color="color"
+              :indeterminate="true"
+              class="ma-0"
+              height="4"
+            >
+            </v-progress-linear>
+          </v-row>
+          <v-row v-if="selectedImage">
+            <v-col cols="12" sm="12" >
+              <div class="text-xs-center">
+                <v-img :src="`${selectedImage}`"  max-width="500" max-height="300"></v-img>
+              </div>
             </v-col>
+            <v-slider
+              v-model="weirdness"
+              step="10"
+              ticks="always"
+              tick-size="2"
+              @change="searchGiphy"
+            ></v-slider>
           </v-row>
         </v-col>
         <v-col cols="12" sm="6">
@@ -43,20 +63,26 @@ export default {
     return {
       appName : this.$store.getters.counter,
       SearchQuery:[],
-      selectedImage: null,
+      color : 'red',
+      searchLoading : false,
+      selectedImage: '',
+      lazySelectedImage : '',
+      weirdness : '1',
     }
   },
   methods : {
     searchGiphy (e) {
+      this.searchLoading = true;
       let query = this.SearchQuery.trim()
+      let weirdness = this.weirdness / 10
       if (query.length > 3){
-        this.$axios.get(`translate?api_key=${this.$store.getters.apiKey}&s=${query}&weirdness=2`).then((response) => {
+        this.$axios.get(`translate?api_key=${this.$store.getters.apiKey}&s=${query}&weirdness=${weirdness}`).then((response) => {
           if (response){
+            this.searchLoading = false;
             let Images = response.data.data.images;  
-            this.selectedImage = Images.downsized_large.url;
-            console.log(this.selectedImage)
+            this.selectedImage = Images.downsized.url;
+            this.lazySelectedImage = Images.fixed_height_small_still.url;
             this.$store.commit('addGif',this.selectedImage);
-            
           }
         }).catch((err) => {
           console.log(err);
