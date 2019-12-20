a4d9cbfb7bd14fc436c0cdde56c9104713e82dd8
fahad-cb <adventivepk@gmail.com>
2019-12-20 11:32:23 +0500

    added : results route added in project

---
+++ pages/results.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-content>
+    <v-container  >
+      <v-row >
+        <v-col cols="12" sm="6" vs-type="flex">
+          THis is results page
+        </v-col>
+        <v-col cols="12" sm="6">
+          
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-content>
+</template>
+<script>
+import { mapMutations } from 'vuex'
+
+export default {
+  data () {
+    return {
+      appName : 'Weirdness calculator',
+    }
+  },
+  methods : {
+    
+  },
+  beforeMount () {
+  },
+  mounted () {
+    console.log(this.$store.getters)
+    this.$myFunction('Results page')
+  }
+}
+
+
+  
+</script>
